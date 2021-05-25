33ea78575c92a522b3f71477ca32cb98872a38ca
brunocabralg35 <brunocabral035@gmail.com>
2021-05-25 15:37:00 -0300

    changed API_KEY and created a .env to keep it from github, created a .env_sample to help others

--- src/components/WeatherContext.js
+++ src/components/WeatherContext.js
@@ -1,23 +1,31 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
+
+const api_key = process.env.REACT_APP_API_KEY;
 
 export const WeatherContext = createContext();
 
 export function WeatherProvider(props) {
-  let url = "https://api.hgbrasil.com/weather?key=426890c0&user_ip=remote";
+  let url = `https://api.hgbrasil.com/weather?key=${api_key}&city_name=Goiania,GO`;
 
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setClima({ cidade: data.results.city, temperatura: data.results.temp, condicao: data.results.condition_code });
-    })
-    .catch((err) => console.log(err));
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setClima({
+          cidade: data.results.city,
+          temperatura: data.results.temp,
+          condicao: data.results.condition_code,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const [clima, setClima] = useState({
     cidade: null,
     temperatura: null,
-    condicao: null
+    condicao: null,
   });
 
   return (
